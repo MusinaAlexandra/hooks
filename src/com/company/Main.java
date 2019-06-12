701c1e37fc43816f8d691eb0e475d1eaa4713c7d
Саша <gorgonova@bk.ru>
2019-06-13 02:54:33 +0500

    count of hooks

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,24 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(checkHooks("()"));
+        System.out.println(checkHooks("((()))"));
         System.out.println(checkHooks("(()"));
         System.out.println(checkHooks(")("));
     }
 
-    public static boolean checkHooks(String line) {
+    public static String checkHooks(String line) {
         int check = 0;
+        int count = 0;
         for (char ch:line.toCharArray()) {
             if (ch == '(') {
                 check++;
+                count++;
             }
             if (ch == ')') {
                 check--;
             }
             if (check < 0) {
-                return false;
+                return "error";
             }
         }
-        return check == 0;
+        return check == 0 ? (count + "") : "error";
     }
 }
